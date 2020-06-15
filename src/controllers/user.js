7d92a99a287b36d518001c6d20dd72a7797b275d
Pawel <jankowskipawelxd@gmail.com>
2020-06-15 18:00:39 +0200

    Make CRUD for user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import User from '../models/User.js'
+import { hashPassword } from '../services/password.js'
 
 const router = new express.Router()
 
@@ -9,16 +10,55 @@ router.get('/users', async (req, res) => {
 })
 
 router.post('/users', async (req, res) => {
-	console.log(req.body)
+	const data = req.body
 
-	const user = new User({
-		username: req.body.username,
-		hashedPassword: req.body.password,
-		email: req.body.email
-	})
+	if(data.username && data.password && data.email) {
+		const user = new User({
+			username: data.username,
+			hashedPassword: hashPassword(data.password),
+			email: data.email
+		})
+	
+		await user.save()
+		res.send(user)
+	}
+	else res.send('Incorrect input data structure')
+})
 
-	await user.save()
+router.get('/users/:id', async (req, res) => {
+	const user = await User.findOne({ _id: req.params.id })
 	res.send(user)
+})
+
+router.delete('/users/:id', async (req, res) => {
+	try {
+		await User.deleteOne({ _id: req.params.id })
+    res.status(204).send()
+	} catch {
+		res.status(404)
+		res.send({ error: "User doesn't exist!" })
+	}
+})
+
+router.patch('/users/:id', async (req, res) => {
+	try {
+		const user = await User.findOne({ _id: req.params.id })
+
+		if(req.body.username)
+			user.username = req.body.username
+
+		if(req.body.password)
+			user.hashedPassword = hashPassword(req.body.password)
+
+		if(req.body.email)
+			user.email = req.body.email
+
+		await user.save()
+		res.send(user)
+	} catch {
+		res.status(404)
+		res.send({ error: "User does not exist" })
+	}
 })
 
 export default router
