79b98fc3ac9f83341bde929f32d24003772c8717
Pawel <jankowskipawelxd@gmail.com>
2020-06-15 10:01:56 +0200

    Add database connection

---
+++ src/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const config = {
+	uris: 'mongodb://localhost:27017/blog',
+	options: {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+}
+
+mongoose.connect(config.uris, config.options)
+
+mongoose.connection.once('open', () => console.info('Connected to database'))
+mongoose.connection.on('error', error => console.error(error))
