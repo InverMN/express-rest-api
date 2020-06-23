8e379e26eec288999d81c495726a1b1e5ffe6ace
Pawel <jankowskipawelxd@gmail.com>
2020-06-23 17:08:57 +0200

    Modify User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,49 @@
 import mongoose from 'mongoose'
+import { email } from '../patterns.js'
 const Schema = mongoose.Schema
 
+//Declare schema
 const data = {
-	username: String,
-	hashedPassword: String,
-	email: String,
-	isModerator: Boolean
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		minlength: 2,
+		maxlength: 16,
+		trim: true
+	},
+	hashedPassword: {
+		type: String,
+		required: true
+	}, 
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+		trim: true,
+		match: email
+	},
+	isVerified: {
+		type: Boolean,
+		default: false
+	},
+	isModerator: { 
+		type: Boolean,
+		default: false
+	},
+	avatar: {
+		type: String
+	}
 }
 
-export default mongoose.model('user', new Schema(data))
+const schema = new Schema(data)
+
+//Add middleware
+//Create avatar image path from document's id
+schema.pre('save', function() {
+	this.avatar = `${process.env.STATIC_PATH}/avatars/${this._id}.webp`
+})
+
+//Create and export model
+export default mongoose.model('user', schema)
