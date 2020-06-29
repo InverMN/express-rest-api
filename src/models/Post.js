4250b0a5407367d765901693774902aade74999f
Pawel <jankowskipawelxd@gmail.com>
2020-06-29 08:30:38 +0200

    Modify Post schema

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,13 +1,52 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
+const basicData = {
+	author: String,
+	owner: {
+		type: 'ObjectId',
+		ref: 'User'
+	},
+	createdAt: { 
+		type: Date,
+		default: Date.now
+	},
+	reactions: {
+		positive: [mongoose.SchemaTypes.ObjectId],
+		negative: [mongoose.SchemaTypes.ObjectId],
+		sum: {
+			type: Number,
+			default: 0
+		}
+	},
+	body: {
+		type: String,
+		required: true
+	}
+}
+
+
+const subcommentData = {
+	...basicData
+}
+
+const subcomment = new Schema(subcommentData)
+
+const commentData = {
+	...basicData,
+	subcomments: [subcomment]
+}
+
+const comment = new Schema(commentData)
+
 const data = {
-	title: String,
-	body: String,
-	// owner: {
-	// 	types: Schema.Types.ObjectId,
-	// 	ref: 'User'
-	// }
+	...basicData,
+	title: {
+		type: String,
+		maxlength: 50,
+		required: true
+	},
+	comments: [comment]
 }
 
 export const Post = mongoose.model('Post', new Schema(data))
