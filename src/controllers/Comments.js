e3be32aa2bbe461ac5a514acb0fa381bf900ba27
Pawel <jankowskipawelxd@gmail.com>
2020-07-01 17:40:49 +0200

    Limit nesting of comments to two levels

--- src/controllers/Comments.js
+++ src/controllers/Comments.js
@@ -38,6 +38,13 @@ Comments.post('/comments/:id', Secure.USER ,async (req, res) => {
 
 		let target = await Post.findById(targetId) || await Comment.findById(targetId)
 
+		console.log(target.collection.name === 'comments')
+		if(target.collection.name === 'comments') {
+			let superParentComment = await Comment.findOne({ replies: { $in: [target._id] } })
+			if(superParentComment !== null)
+				target = superParentComment
+		}
+
 		target.replies.push(comment._id)
 		comment.save()
 		target.save()
