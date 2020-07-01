09b7ebef128d9869781f5ec7217a18473839e6f3
Pawel <jankowskipawelxd@gmail.com>
2020-07-01 11:44:09 +0200

    Add possibility to append reaction to single document

--- src/services/documentModifiers.js
+++ src/services/documentModifiers.js
@@ -1,19 +1,24 @@
 import { Feedback } from '../models/Feedback.js'
 
-export async function appendUserReaction(documents, userId) {
-	return await Promise.all(documents.map(async singleDocument => {
-		const data = await { ...singleDocument._doc }
+export async function appendUserReaction(input, userId) {
+	if(input instanceof Array) 
+		return await Promise.all(input.map(async singleDocument => await appendUserReactionToSingle(singleDocument, userId)))
+	else 
+		return await appendUserReactionToSingle(input, userId)
+}
 
-		const feedbackId = data.popularity.feedback
-		const feedback = await Feedback.findById(feedbackId)
+async function appendUserReactionToSingle(document, userId) {
+	const data = { ...document._doc }
+	
+	const feedbackId = data.popularity.feedback
+	const feedback = await Feedback.findById(feedbackId)
 
-		if(feedback.positive.includes(userId))
-			data.userReaction = 'positive'
-		else if(feedback.negative.includes(userId))
-			data.userReaction = 'negative'
-		else 
-			data.userReaction = 'none'
+	if(feedback.positive.includes(userId))
+		data.userReaction = 'positive'
+	else if(feedback.negative.includes(userId))
+		data.userReaction = 'negative'
+	else 
+		data.userReaction = 'none'
 
-		return data
-	}))
+	return data
 }
