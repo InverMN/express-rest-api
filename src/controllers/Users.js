a35b91914f96e3c6b1beead0ae04d20c3acd7638
Pawel <jankowskipawelxd@gmail.com>
2020-07-02 15:46:45 +0200

    Handle errors in /users request

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,52 +1,70 @@
 import { User } from '../models/index.js'
 import { Secure } from '../middleware/index.js'
 import { hashPassword } from '../services/password.js'
-import { Controller } from './common/index.js'
+import { Controller, update } from './common/index.js'
 
 export const Users = new Controller()
 
 Users.get('/users', Secure.MODERATOR, async (req, res) => {
 	const users = await User.find()
-	res.json(users)
+	res.send(users)
 })
 
 Users.post('/users', async (req, res) => {
-	const data = req.body
+	const { username, password, email } = req.body
 
-	if(data.username && data.password && data.email) {
-		const user = new User({
-			username: data.username,
-			hashedPassword: hashPassword(data.password),
-			email: data.email
-		})
-	
-		await user.save()
-		res.send(user)
-	}
-	else res.send('Incorrect input data structure')
+	if(password === undefined) 
+		throw 'missing password'
+
+	if(password.length <= 5)
+		throw 'short password excepted 6'
+
+	if(password.length >= 199)
+		throw 'long password excepted 200'
+
+	const user = new User({ email, hashedPassword: hashPassword(password), username })
+	await user.save()
+	res.send(user)
 })
 
 Users.get('/users/:id', async (req, res) => {
 	const user = await User.findOne({ _id: req.params.id })
+	if(user === null)
+		throw 'not found user'
+
 	res.send(user)
 })
 
 Users.delete('/users/:id', async (req, res) => {
-	await User.deleteOne({ _id: req.params.id })
-	res.status(204).send()
+	const user = await User.findOne({ _id: req.params.id })
+	if(user === null)
+		throw 'not found user'
+
+	user.remove()
+	res.send(user)
 })	
 
 Users.patch('/users/:id', Secure.OWNER, async (req, res) => {
 	const user = await User.findOne({ _id: req.params.id })
+	if(user === null)
+		throw 'not found user'
 
-	if(req.body.username)
-		user.username = req.body.username
+	if('password' in req.body) {
+		const { password } = req.body
 
-	if(req.body.password)
-		user.hashedPassword = hashPassword(req.body.password)
+		if(password === undefined) 
+			throw 'missing password'
+	
+		if(password.length <= 5)
+			throw 'short password excepted 6'
+	
+		if(password.length >= 199)
+			throw 'long password excepted 200'
 
-	if(req.body.email)
-		user.email = req.body.email
+		req.body.hashedPassword = hashPassword(password)
+	}
+
+	update(user, req.body, ['username','email','hashedPassword'])
 
 	await user.save()
 	res.send(user)
