174ae898e557bdfc82e8186f8c598d249298d70f
Pawel <jankowskipawelxd@gmail.com>
2020-07-03 21:44:36 +0200

    Leave one level of auto-generated avatar quality

--- src/services/createIdenticon.js
+++ src/services/createIdenticon.js
@@ -2,15 +2,7 @@ import Files from 'fs'
 import Avatar from 'avatar-builder'
 
 export async function createIdenticon(userId) {
-	{
-		const builder = Avatar.identiconBuilder(256)
-		const identicon = await builder.create(userId)
-		Files.writeFileSync(`public/avatars/medium/${userId}.png`, identicon)
-	}
-	{
-		//create 64x64 version of the same identicon
-		const builder = Avatar.identiconBuilder(256)
-		const identicon = await builder.create(userId)
-		Files.writeFileSync(`public/avatars/low/${userId}.png`, identicon)
-	}
+	const builder = Avatar.identiconBuilder(256)
+	const identicon = await builder.create(userId)
+	Files.writeFileSync(`public/avatars/${userId}.png`, identicon)
 }
