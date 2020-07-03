874d1d2a2791acefaa58cdf1614a4732442fe34a
Pawel <jankowskipawelxd@gmail.com>
2020-07-03 15:37:28 +0200

    Append to api url "/api/v1"

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ export function run(method = 'production') {
 		
 		/* Add controllers to app */
 		for(const controller in Controllers) {
-			app.use('/', Controllers[controller].router)
+			app.use('/api/v1', Controllers[controller].router)
 		}
 		
 		/* Start listening */
