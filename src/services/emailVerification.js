f473e5cea7c40ae3628168131a9f7e2d42887b0d
Pawel <jankowskipawelxd@gmail.com>
2020-07-03 16:02:30 +0200

    Add production e-mail verification

--- src/services/emailVerification.js
+++ src/services/emailVerification.js
@@ -4,36 +4,54 @@ import nodemailer from 'nodemailer'
 let lazyTransport = null
 
 export async function createConfirmationTransport() {
-	let testAccount = await nodemailer.createTestAccount()
-	
-	const transportOptions = {
-		host: 'smtp.ethereal.email', 
-		port: 587,
-		secure: false,
-		auth: {
-			user: testAccount.user, 
-			pass: testAccount.pass
+	if(process.env.MODE === 'development') {
+		let testAccount = await nodemailer.createTestAccount()
+		
+		const transportOptions = {
+			host: 'smtp.ethereal.email', 
+			port: 587,
+			secure: false,
+			auth: {
+				user: testAccount.user, 
+				pass: testAccount.pass
+			}
 		}
-	}
+	
+		const emailDefaults = {
+			from: 'dev@example.com',
+			subject: 'Confirm Email ✔'
+		}
+	
+		return nodemailer.createTransport(transportOptions, emailDefaults)
+	} else {
+		const transportOptions = {
+			service: 'gmail',
+			auth: {
+				user: process.env.EMAIL_USER, 
+				pass: process.env.EMAIL_PASSWORD
+			}
+		}
 
-	const emailDefaults = {
-		from: 'dev@example.com',
-		subject: 'Confirm Email ✔'
-	}
+		const emailDefaults = {
+			from: 'dev@example.com',
+			subject: 'Confirm Email ✔'
+		}
 
-	return nodemailer.createTransport(transportOptions, emailDefaults)
+		return nodemailer.createTransport(transportOptions, emailDefaults)
+	}
 }
 
 export async function sendConfirmationEmail(userId, email) {
 	const emailToken = await generateEmailConfirmationToken(userId)
-	const url = `http://${process.env.ADDRESS}/confirm/${emailToken}`
+	const url = `http://${process.env.ADDRESS}/api/v1//confirm/${emailToken}`
 
 	if(lazyTransport === null) lazyTransport = await createConfirmationTransport()
 
-	lazyTransport.sendMail({
+	const info = await lazyTransport.sendMail({
 		to: email,
 		html: `Please click <a href="${url}">this link</a> to confirm your e-mail.`
-	}).then( info => {
-		console.log('verify email message:', nodemailer.getTestMessageUrl(info))
 	})
+
+	if(process.env.MODE === 'development')
+		console.log('verify email message:', nodemailer.getTestMessageUrl(info))
 }
