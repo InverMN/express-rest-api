0c8a89ccc0a97421b07a055083e791d992eba38f
Pawel <jankowskipawelxd@gmail.com>
2020-07-04 14:10:02 +0200

    Allow app to be exported

--- src/app.js
+++ src/app.js
@@ -6,32 +6,29 @@ import { openDatabase, openTestDatabase } from './database.js'
 import * as Controllers from './controllers/index.js'
 
 export function run(method = 'production') {
-	let database
+	if(method === 'production') openDatabase() 
+	else openTestDatabase()
 
-	if(method === 'production') database = openDatabase() 
-	else database = openTestDatabase()
+	dotenv.config()
 
-	database.once('open', () => {
-		dotenv.config()
+	const app = express()
+
+	app.use(bodyParser.json())
+	app.use(cookieParser())
+
+	app.use('/static', express.static('public'))
+	app.get('/', (_, res) => res.send('Homepage'))
 	
-		const app = express()
+	/* Add controllers to app */
+	for(const controller in Controllers) {
+		app.use('/api/v1', Controllers[controller].router)
+	}
 	
-		app.use(bodyParser.json())
-		app.use(cookieParser())
+	/* Start listening */
+	const port = parseInt(process.env.PORT)
+	console.info(`Listening on localhost:${port}`)
 	
-		app.use('/static', express.static('public'))
-		app.get('/', (_, res) => res.send('Homepage'))
-		
-		/* Add controllers to app */
-		for(const controller in Controllers) {
-			app.use('/api/v1', Controllers[controller].router)
-		}
-		
-		/* Start listening */
-		const port = parseInt(process.env.PORT)
-		console.info(`Listening on localhost:${port}`)
-		
-		app.server = app.listen(port)
-		return app
-	})
+	app.server = app.listen(port)
+
+	return app
 }
