ed1481ad42e35cef857fecfb26c7e42aec88df74
Pawel <jankowskipawelxd@gmail.com>
2020-07-28 14:17:58 +0200

    Create Notification model

---
+++ src/models/Notification.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+import { Author as User, NotificationSubjects } from './common/index.js'
+
+const data = {
+	sender: User,
+	receiver: {
+		type: User,
+		required: true
+	},
+	subject: {
+		type: NotificationSubjects,
+		required: true
+	},
+	data: Object,
+	checked: {
+		type: Boolean,
+		default: false
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
+}
+
+export const Notification = mongoose.model('Notification', new Schema(data))
