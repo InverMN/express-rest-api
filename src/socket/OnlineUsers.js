1418ee202e2ba12a3cc757c5e3a3ad48f26ea69c
Pawel <jankowskipawelxd@gmail.com>
2020-07-30 14:43:11 +0200

    Add operations for adding, getting and deleting users

--- src/socket/OnlineUsers.js
+++ src/socket/OnlineUsers.js
@@ -2,4 +2,16 @@ export class OnlineUsers {
 	constructor() {
 		this.users = []
 	}
+
+	add(newUser) {
+		this.users.push(newUser)
+	}
+
+	get(userId) {
+		return this.users.find(user => user.id === userId)
+	}
+
+	remove(userId) {
+		this.users = this.users.filter(user => user.id !== userId)
+	}
 }
