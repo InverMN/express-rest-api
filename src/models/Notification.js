13b79ba946514e410efa8f82adc700e9476063d2
Pawel <jankowskipawelxd@gmail.com>
2020-08-01 20:50:50 +0200

    Move notification sending source

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 import { Author as User, NotificationSubjects } from './common/index.js'
-import { server } from '../socket/index.js'
-import { documentToData } from '../controllers/common/index.js'
 
 const data = {
 	sender: User,
@@ -26,16 +24,5 @@ const data = {
 }
 
 const schema = new Schema(data)
-
-const init = async document => {
-	document = documentToData(document)
-	server.sendNotification(document)
-}
-
-schema.pre('save', async function(next) {
-	if(this.isNew)
-		await init(this)
-	next()
-})
 
 export const Notification = mongoose.model('Notification', schema)
--- src/services/notifications.js
+++ src/services/notifications.js
@@ -1,4 +1,6 @@
 import { Notification, User } from '../models/index.js'
+import { server } from '../socket/index.js'
+import { documentToData } from '../controllers/common/index.js'
 
 export const createNotification = async (senderId, receiverId, subject, data) => {
 	const [sender, receiver] = await Promise.all([ User.findById(senderId), User.findById(receiverId) ])
@@ -11,4 +13,5 @@ export const createNotification = async (senderId, receiverId, subject, data) =>
 	})
 
 	notification.save()
+	server.sendNotification(documentToData(notification))
 }
