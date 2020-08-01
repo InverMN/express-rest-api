9faff018a3b808be08cf502af01dabc1eb0b7bef
Pawel <jankowskipawelxd@gmail.com>
2020-08-01 16:25:48 +0200

    Add endpoint for deleting Notifications

--- src/controllers/Notifications.js
+++ src/controllers/Notifications.js
@@ -8,3 +8,10 @@ Notifications.get('/notifications', Secure.USER, async (req, res) => {
 	const notifications = await Notification.find({ receiver: { id: req.user._id } })
 	res.send(documentToData(notifications))
 })
+
+Notifications.delete('/notifications/:id', Secure.OWNER, async (req, res) => {
+	const notification = await Notification.findById({ _id: req.params.id })
+	req.verifyOwnership(notification.receiver.id)
+	notification.remove()
+	res.send(documentToData(notification))
+})
